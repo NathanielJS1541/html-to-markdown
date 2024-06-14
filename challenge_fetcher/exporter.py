852306d864a157efca08b95ce497a4dc1b84dba3
Nathaniel Struselis <nathaniel.struselis@hotmail.co.uk>
2024-06-14 12:37:45 +0100

    Added generate_padded_number method to exporter.py

--- challenge_fetcher/exporter.py
+++ challenge_fetcher/exporter.py
@@ -1,3 +1,6 @@
+import enum
+
+
 # Desired name for the desired MarkDown file inside the challenge folder.
 README_NAME = "README.md"
 
@@ -20,3 +23,18 @@ StatusMessages = {
     ExportStatus.ALREADY_EXISTS: f"{README_NAME} already exists... Skipping!",
     ExportStatus.OUTPUT_DIR_IS_FILE: "Error! The specified output directory is a file.",
 }
+
+
+def generate_padded_number(number: int, total_digits: int) -> str:
+    """generate_padded_number Convert a number to a string and pad it to the specified number of digits.
+
+    Args:
+        number (int): The number to apply padding to.
+        total_digits (int): The number of digits to pad the number to.
+
+    Returns:
+        str: The number, padded to total_digits, as a string.
+    """
+
+    # Use string.zfill() to pad the number with zeros to the left.
+    return str(number).zfill(total_digits)
