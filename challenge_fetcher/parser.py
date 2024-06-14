9dd39acd2245464529ea8aba0c76ca19c60b70d5
Nathaniel Struselis <nathaniel.struselis@hotmail.co.uk>
2024-06-14 10:29:11 +0100

    Added ABOUT_URL_REGEX to parser.py.
    This regex captures relative URLs to Project Euler's "about" pages.
    
    These will not be downloaded, and will be left as web links.

--- challenge_fetcher/parser.py
+++ challenge_fetcher/parser.py
@@ -5,6 +5,7 @@ import re
 CHALLENGE_URL_REGEX is a compiled regular expression that matches strings of the form "problem=<number>":
   - "^" asserts the start of a line.
   - "problem=" matches the literal string problem=.
+  - "problem=" matches the literal string "problem=".
   - "(?P<number>\d+)" is a named capturing group to capture one or more digits:
     - "(...)" defines the capture group.
     - "?P<number>" names the capture group "number".
@@ -15,6 +16,7 @@ CHALLENGE_URL_REGEX = re.compile(r"^problem=(?P<number>\d+)")
 
 """
 RESOURCE_URL_REGEX is a compiled regular expression that matches strings of the form "resources/<filename>" and "project/resources/<filename>:
+RESOURCE_URL_REGEX is a compiled regular expression that matches strings of the form "resources/<filename>" and "project/resources/<filename>":
   - "^" asserts the start of a line.
   - "(project\/)?" is a capture group that matches the optional literal string "project/".
     - "(...)" defines the capture group.
@@ -24,6 +26,7 @@ RESOURCE_URL_REGEX is a compiled regular expression that matches strings of the 
     - "(...)" defines the capture group.
     - "." matches any character (except for a newline).
     - "+" is a greedy quantifier that matches one or more times, and allows the "." to capture one or more digits.
+    - "+" is a greedy quantifier that matches one or more times, and allows the previous character class to capture one or more word characters.
     - "\/" matches the literal "/".
     - "?" makes the capture group optional.
   - "(?P<filename>.+)" is a named capturing group to match any characters:
@@ -31,6 +34,18 @@ RESOURCE_URL_REGEX is a compiled regular expression that matches strings of the 
     - "?P<filename>" names the capture group "filename".
     - "." matches any character (except for a newline).
     - "+" is a greedy quantifier that matches one or more times, and allows the "." to capture one or more digits.
+    - "+" is a greedy quantifier that matches one or more times, and allows the previous character class to capture one or more word characters.
 """
 RESOURCE_URL_REGEX = re.compile(r"^(project\/)?resources\/(.+\/)?(?P<filename>.+)")
+
+"""
+ABOUT_URL_REGEX is a compiled regular expression that matches strings of the form "about=<word>":
+  - "^" asserts the start of a line.
+  - "about=" matches the literal string "about=".
+  - "(\w+)" is a capturing group to capture one or more word characters:
+    - "(...)" defines the capture group.
+    - "\w" is a shorthand character class to capture word characters (a-z, A-Z, 0-9, _).
+    - "+" is a greedy quantifier that matches one or more times, and allows the previous character class to capture one or more word characters.
+"""
+ABOUT_URL_REGEX = re.compile(r"^about=(\w+)")
 
