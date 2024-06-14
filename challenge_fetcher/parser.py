87111d2632b12262ca8cc51448e2508ae3fb7ef1
Nathaniel Struselis <nathaniel.struselis@hotmail.co.uk>
2024-06-14 10:45:55 +0100

    Merged image and resource Regexes in parser.py.

--- challenge_fetcher/parser.py
+++ challenge_fetcher/parser.py
@@ -4,7 +4,6 @@ import re
 """
 CHALLENGE_URL_REGEX is a compiled regular expression that matches strings of the form "problem=<number>":
   - "^" asserts the start of a line.
-  - "problem=" matches the literal string problem=.
   - "problem=" matches the literal string "problem=".
   - "(?P<number>\d+)" is a named capturing group to capture one or more digits:
     - "(...)" defines the capture group.
@@ -15,17 +14,21 @@ CHALLENGE_URL_REGEX is a compiled regular expression that matches strings of the
 CHALLENGE_URL_REGEX = re.compile(r"^problem=(?P<number>\d+)")
 
 """
-RESOURCE_URL_REGEX is a compiled regular expression that matches strings of the form "resources/<filename>" and "project/resources/<filename>:
-RESOURCE_URL_REGEX is a compiled regular expression that matches strings of the form "resources/<filename>" and "project/resources/<filename>":
+RESOURCE_URL_REGEX is a compiled regular expression that matches strings of the form "resources/<optional_path>/<filename>",
+"project/resources/<optional_path>/<filename>", "images/<optional_path>/<filename>", and "project/images/<optional_path>/<filename>":
   - "^" asserts the start of a line.
   - "(project\/)?" is a capture group that matches the optional literal string "project/".
     - "(...)" defines the capture group.
     - "?" makes the capture group optional.
-  - "resources\/" matches the literal string "resources/".
+  - "(resources|images)" matches either the literal string "resources" or "images".
+    - "(...)" defines the capture group.
+    - "resources" matches the literal string "resources".
+    - "|" is the OR operator.
+    - "images" matches the literal string "images".
+  - "\/" matches the literal string "/".
   - "(.+\/)?" is an optional capture group to match any characters followed by a "/":
     - "(...)" defines the capture group.
     - "." matches any character (except for a newline).
-    - "+" is a greedy quantifier that matches one or more times, and allows the "." to capture one or more digits.
     - "+" is a greedy quantifier that matches one or more times, and allows the previous character class to capture one or more word characters.
     - "\/" matches the literal "/".
     - "?" makes the capture group optional.
@@ -33,10 +36,9 @@ RESOURCE_URL_REGEX is a compiled regular expression that matches strings of the 
     - "(...)" defines the capture group.
     - "?P<filename>" names the capture group "filename".
     - "." matches any character (except for a newline).
-    - "+" is a greedy quantifier that matches one or more times, and allows the "." to capture one or more digits.
     - "+" is a greedy quantifier that matches one or more times, and allows the previous character class to capture one or more word characters.
 """
-RESOURCE_URL_REGEX = re.compile(r"^(project\/)?resources\/(.+\/)?(?P<filename>.+)")
+RESOURCE_URL_REGEX = re.compile(r"^(project\/)?(resources|images)\/(.+\/)?(?P<filename>.+)")
 
 """
 ABOUT_URL_REGEX is a compiled regular expression that matches strings of the form "about=<word>":
@@ -49,22 +51,4 @@ ABOUT_URL_REGEX is a compiled regular expression that matches strings of the for
 """
 ABOUT_URL_REGEX = re.compile(r"^about=(\w+)")
 
-# TODO: Maybe this could be combined with RESOURCE_URL_REGEX?
-"""
-IMAGES_URL_REGEX is a compiled regular expression that matches strings of the form "project/images/<optional_path>/<filename>":
-  - "^" asserts the start of a line.
-  - "project/images/" matches the literal string "project/images/".
-  - "(.+\/)?" is an optional capturing group to capture any characters followed by a "/":
-    - "(...)" defines the capture group.
-    - "." matches any character (except for a newline).
-    - "+" is a greedy quantifier that matches one or more times, and allows the previous character class to capture one or more word characters.
-    - "\/" matches the literal "/".
-    - "?" makes the capture group optional.
-  - "(?P<filename>.+)" is a named capturing group to match any characters:
-    - "(...)" defines the capture group.
-    - "?P<filename>" names the capture group "filename".
-    - "." matches any character (except for a newline).
-    - "+" is a greedy quantifier that matches one or more times, and allows the previous character class to capture one or more word characters.
-"""
-IMAGES_URL_REGEX = re.compile(r"^project\/images\/(.+\/)?(?P<filename>.+)")
 
