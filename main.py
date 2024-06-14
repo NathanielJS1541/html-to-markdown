cf73687072218f1371906649fc53060ea4eb9144
Nathaniel Struselis <nathaniel.struselis@hotmail.co.uk>
2024-06-14 09:34:44 +0100

    Added webscraper for challenges
    Added a scraper to get the response from a HTTP get request to the specified challenge number's URL.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import challenge_fetcher
 import challenge_fetcher.arguments
+import challenge_fetcher.scraper
 
 # Get the command-line arguments using argparse.
 args = challenge_fetcher.arguments.parse_arguments()
@@ -7,5 +8,17 @@ args = challenge_fetcher.arguments.parse_arguments()
 # Ensure the arguments are valid before running the script.
 if challenge_fetcher.arguments.validate_arguments(args):
     challenge_range = range(args.start_challenge, args.end_challenge + 1)
+
+    # Work out from the largest challenge number how many digits should be created for the folder name.
+    folder_num_digits = len(str(args.end_challenge))
+
+    # Loop through all the challenge numbers in the selected range.
+    for challenge_number in challenge_range:
+        # Get the HTML response from the ProjectEuler page for the challenge number.
+        contents = challenge_fetcher.scraper.get_content(challenge_number)
+
+        # Skip this challenge if the scraper couldn't return the contents.
+        if contents is None:
+            continue
 else:
     print("Input argument validation failed... Exiting.")
